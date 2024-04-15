28e8c2baf0d85ffedbae21ec720422c2e5f1a65b
Lissa2121 <114819609+Lissa2121@users.noreply.github.com>
2024-04-15 11:49:23 +0300

    hw2

--- js/task-1.js
+++ js/task-1.js
@@ -13,9 +13,10 @@ const transfer_third = makeTransaction(10, 5000, 8000);
 const transfer_fourth = makeTransaction(8, 2000, 10000);
 const transfer_fifth = makeTransaction(10, 500, 5000);
 
-console.log(transfer_first); 
-console.log(transfer_second); 
-console.log(transfer_third); 
-console.log(transfer_fourth); 
-console.log(transfer_fifth);
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
 
--- js/task-4.js
+++ js/task-4.js
@@ -24,8 +24,9 @@ const Australia = getShippingCost("Australia", 170);
 const Jamaica = getShippingCost("Jamaica", 120);
 const Sweden = getShippingCost("Sweden");
 
-console.log(China);
-console.log(Chile);
-console.log(Australia);
-console.log(Jamaica);
-console.log(Sweden);
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
